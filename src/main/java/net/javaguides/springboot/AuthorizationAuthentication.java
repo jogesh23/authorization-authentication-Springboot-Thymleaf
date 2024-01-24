17e28d453829abaf46b24c21c6d74e0d4348cceb
jogesh <jogeshpedapalli@gmail.com>
2024-01-24 10:40:54 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot/AuthorizationAuthentication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationAuthentication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationAuthentication.class, args);
+	}
+
+}
